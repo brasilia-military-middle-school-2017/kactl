ec4c36f237e6f8df9fc49ed7fa76cd413ecc7610
Arthur Botelho <arthur.botelho@incognia.com>
2026-03-01 19:47:55 -0300

    removing more

--- content/data-structures/Spec.h
+++ content/data-structures/Spec.h
@@ -1,44 +1,9 @@
 /**
  * Author: Arthur Botelho
- * Description: Algebraic structures for RQDS. Includes Ruan's version.
+ * Description: Spec for Seg Beats. It's similar to other data structures.
  * Time: varies
  * Status: ok
  */
-
-struct Group { //sum
-	using T = int;
-	static constexpr T id = 0;
-	static T op(T a, T b){return a+b;}
-	static T inv(T a){return -a;}
-};
-
-struct LazySpecRuan { //sum
-	using S = int;
-	using K = int;
-	static S op(S a, S b) { return max(a, b); }
-	static S update(K f, S a) { return f + a; }
-	static K compose(const K f, const K g) { return f + g; }
-	static S id() { return 0; }
-};
-
-struct LazySpecArthur { //set add sum
-	using T = ll;
-	using L = pair<int, ll>;
-	static constexpr T id = 0;
-	static T op(T a, T b){return a + b;}
-	static T ch(T past, L upd, int lx, int rx){
-		ll s = rx-lx;
-		auto [t, x] = upd;
-		if (t)return s*x;
-		else return past+s*x;
-	}
-	static L cmp(L cur, L upd){
-		auto [t1, x1] = cur;
-		auto [t2, x2] = upd;
-		if (t2)return upd;
-		else return {t1, x1+x2};
-	}
-};
 
 struct node{int max1,max2,maxc; ll sum;};
 struct BeatsSpec{ //chmin sum
