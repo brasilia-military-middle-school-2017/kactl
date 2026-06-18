a77b75b6cff17c7bad176307071287d5d201a252
Arthur Botelho <arthur.botelho@incognia.com>
2026-06-18 16:35:51 -0300

    update todo, remove troubleshoot and update seg

--- content/data-structures/SegmentTree.h
+++ content/data-structures/SegmentTree.h
@@ -1,27 +1,25 @@
 /**
- * Author: Ruan Petrus
- * Description: Iterative SegTree
- * Can be changed by modifying Spec
+ * Author: Arthur Botelho
+ * Description: Half-Open Iterative SegTree
  * Time: O(\log N)
- * Status: stress-tested
+ * Status: not tested
  */
 #pragma once
 
-template<typename LS> struct SegTree {
-	using S = typename LS::S;
-	using K = typename LS::K;
-	int n; vector<S> seg;
-	SegTree(int _n) : n(_n), seg(2*n, LS::id()) {}
-	void update(int no, K val) {
-		no += n; seg[no] = LS::update(val, seg[no]);
-		while (no > 1) no /= 2, seg[no] = LS::op(seg[no*2], seg[no*2+1]);
+template<class S> struct SegTree{
+	using T = typename S::T;
+	int n; vector<T> seg;
+	SegTree(int s):n(s),seg(2*n,S::id){}
+	void update(T x, int i){
+		for(seg[i+=n]=x;i/=2;)
+			seg[i] = S::op(seg[2*i], seg[2*i+1])
 	}
-	S query(int l, int r) { // [l, r)
-		S vl = LS::id(), vr = LS::id();
-		for (l += n, r += n; l < r; l /= 2, r /= 2) {
-			if (l & 1) vl = LS::op(vl, seg[l++]);
-			if (r & 1) vr = LS::op(seg[--r], vr);
-		}
-		return LS::op(vl, vr);
+	T query(int l, int r){ // [l, r)
+		T vl=S::id, vr=S::id;
+		for(l+=n,r+=n;l<r;l/=2,r/=2){
+			if (l&1)vl = S::op(vl, seg[l++]);
+			if (r&1)vr = S::op(seg[--r], vr);
+		} 
+		return S::op(vl, vr);
 	}
 };
